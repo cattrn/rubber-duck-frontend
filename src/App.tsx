adcc392fef58d6c3eff2201244322830f7c8e5dd
Caterina Turnbull <caterina.turnbull@gmail.com>
2023-05-19 21:20:08 +1000

    Style chat messages
    Co-authored-by: Bunny Ryou <bunnyryou@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import ChatBubble from "./components/ChatBubble"
 
 function App() {
   return (
-    <div className="App max-w-xl mx-auto">
+    <div className="App bg-neutral">
+      <div className="max-w-xl mx-auto h-screen flex flex-col bg-base-200 p-4">
+        <div className="grow">
       <ChatBubble sender="duck" text="Hello there!" />
       <ChatBubble sender="user" text="Hi!" />
       <ChatBubble sender="user" text="How are you?" />
       <ChatBubble sender="duck" text="I am duck!" />
+        </div>
+      </div>
     </div>
   )
 }
